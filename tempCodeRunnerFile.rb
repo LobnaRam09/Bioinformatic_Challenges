60b1b34999916b0cccd0b2607f3e404929a90143
LobnaRam09 <90159414+LobnaRam09@users.noreply.github.com>
2021-10-20 00:03:45 +0200

    Delete tempCodeRunnerFile.rb

--- tempCodeRunnerFile.rb
+++
@@ -1,7 +0,0 @@
-n1.all_stocks.values.each do |seed|
-#     seed.plant(7)
-# end
-
-
-# #to record the new information in a new file after planting 7 grams of each type
-# n1.newrecord("newdata.tsv")
